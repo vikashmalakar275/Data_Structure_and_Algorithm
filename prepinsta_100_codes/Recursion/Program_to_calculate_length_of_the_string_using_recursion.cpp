a310397765364f0e3833bbce0ffd39d6e5a88f54
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-10-05 19:17:05 +0530

    Given a string calculate length of the string using recursion.
    Examples:
    
    Input : str = abcd
    Output :4
    
    Input : str = GEEKSFORGEEKS
    Output :13

--- prepinsta_100_codes/Recursion/Program_to_calculate_length_of_the_string_using_recursion.cpp
+++ prepinsta_100_codes/Recursion/Program_to_calculate_length_of_the_string_using_recursion.cpp
@@ -1,5 +1,13 @@
 /*
+Given a string calculate length of the string using recursion. 
 
+Examples: 
+
+Input : str = "abcd"
+Output :4
+
+Input : str = "GEEKSFORGEEKS"
+Output :13
 */
 
 #include <iostream>
