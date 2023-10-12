4f59989506afe01f317396bbc18ad2e44331f2ea
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-10-13 00:10:30 +0530

    DSA

---
+++ takeUforward/Strivers_A2Z_DSA_Course/Know_Basic_Maths/Print_all_Divisors_of_a_given_Number.cpp
@@ -0,0 +1,14 @@
+/*
+Problem Statement: Given a number, print all the divisors of the number. A divisor is a number that gives the remainder as zero when divided.
+
+Examples
+Example 1:
+Input: n = 36
+Output: 1 2 3 4 6 9 12 18 36
+Explanation: All the divisors of 36 are printed.
+
+Example 2:
+Input: n = 97
+Output: 1 97
+Explanation: Since 97 is a prime number, only 1 and 97 are printed.
+*/
