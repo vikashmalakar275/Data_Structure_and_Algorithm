7338de156f24c7fd2d3eafba7f673bd1e0a790b3
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-10-21 22:18:10 +0530

    array

--- takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Easy/Rotate_array_by_K_elements.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Easy/Rotate_array_by_K_elements.cpp
@@ -17,15 +17,103 @@ Explanation: Array is rotated to right by 3 position.
 #include <iostream>
 using namespace std;
 
-void rotatearray(vector<int> &arr)
+void Rotatetoright(int arr[], int n, int k)
 {
-    
+  if (n == 0)
+    return;
+  k = k % n;
+  if (k > n)
+    return;
+  int temp[k];
+  for (int i = n - k; i < n; i++)
+  {
+    temp[i - n + k] = arr[i];
+  }
+  for (int i = n - k - 1; i >= 0; i--)
+  {
+    arr[i + k] = arr[i];
+  }
+  for (int i = 0; i < k; i++)
+  {
+    arr[i] = temp[i];
+  }
+  cout << "After Rotating the elements to right " << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
 }
 
-void rotate_array_2(vector<int> arr, int n) {
-  
+void Rotatetoleft(int arr[], int n, int k)
+{
+  if (n == 0)
+    return;
+  k = k % n;
+  if (k > n)
+    return;
+  int temp[k];
+  for (int i = 0; i < k; i++)
+  {
+    temp[i] = arr[i];
+  }
+  for (int i = 0; i < n - k; i++)
+  {
+    arr[i] = arr[i + k];
+  }
+  for (int i = n - k; i < n; i++)
+  {
+    arr[i] = temp[i - n + k];
+  }
+  cout << "After Rotating the elements to left " << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
 }
 
+void Reverse(int arr[], int start, int end)
+{
+  while (start <= end)
+  {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+// Function to Rotate k elements to right
+void Rotateeletoright(int arr[], int n, int k)
+{
+  // Reverse first n-k elements
+  Reverse(arr, 0, n - k - 1);
+  // Reverse last k elements
+  Reverse(arr, n - k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}
+
+void Reverse(int arr[], int start, int end)
+{
+  while (start <= end)
+  {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+// Function to Rotate k elements to left
+void Rotateeletoleft(int arr[], int n, int k)
+{
+  // Reverse first k elements
+  Reverse(arr, 0, k - 1);
+  // Reverse last n-k elements
+  Reverse(arr, k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}
 
 int main()
 {
@@ -40,15 +128,12 @@ int main()
         cin>>a;
         arr.push_back(a);
     }
-    cout<<"Array after one left rotate :"<<endl;
-    rotate_array_2(arr,n);
-    cout<<endl;
-    otatearray(arr);
-    cout<<"Array after one left rotate :"<<endl;
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    int k;
+    cout<<"Eneter the number you want rotate :"<<endl;
+    cin>>k;
+    string str;
+    cout<<"Enter you rotate choice eg. left/right :"<<endl;
+    getline(cin,str);
+    cout<<"Array after "<<k<<" left rotate :"<<endl;
     return 0;
 }
