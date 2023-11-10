09fd79ef25e33071f5b5a04462d9fa30c717eeeb
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-11-10 11:17:30 +0530

    array

--- takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Medium/Set_Matrix_Zero.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Medium/Set_Matrix_Zero.cpp
@@ -22,7 +22,11 @@ Explanation:Since matrix[0][0]=0 and matrix[0][3]=0. Therefore 1st row, 1st colu
 #include <iostream>
 using namespace std;
 
-
+//Brute Force Approach
+//Time Complexity: O((N*M)*(N + M)) + O(N*M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+//Reason: Firstly, we are traversing the matrix to find the cells with the value 0. It takes O(N*M). Now, whenever we find any such cell we mark that row and column with -1. This process takes O(N+M). So, combining this the whole process, finding and marking, takes O((N*M)*(N + M)).
+//Another O(N*M) is taken to mark all the cells with -1 as 0 finally.
+//Space Complexity: O(1) as we are not using any extra space.
 void makeRowZero(vector<vector<int> > &arr,int n,int m,int i)
 {
     for(int j=0;j<m;j++)
@@ -43,7 +47,6 @@ void makeColumnZero(vector<vector<int> > &arr,int n,int m,int j)
         }
     }
 }
-
 void matrixZero(vector<vector<int> > &arr,int n,int m)
 {
     for(int i=0;i<n;i++)
@@ -70,6 +73,40 @@ void matrixZero(vector<vector<int> > &arr,int n,int m)
     }
 }
 
+//Better Approach
+//Time Complexity: O(2*(N*M)), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+//Reason: We are traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
+//Space Complexity: O(N) + O(M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+//Reason: O(N) is for using the row array and O(M) is for using the col array.
+vector<vector<int> > matrixZero_1(vector<vector<int> > arr,int n,int m)
+{
+    vector<int> row(n,0);
+    vector<int> column(m,0);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(arr[i][j] == 0)
+            {
+                row[i] = 1;
+                column[j] = 1;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(row[i] || column[j])
+            {
+                arr[i][j] = 0;
+            }
+        }
+    }
+    return arr;
+}
+
 int main()
 {
     int n,m;
@@ -86,6 +123,18 @@ int main()
         {
             cin >> arr[i][j];
         }
+    }
+
+
+    vector<vector<int> > ans = matrixZero_1(arr,n,m);
+    cout<<"The Final Matrix is :"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<"\n";
     }
     matrixZero(arr,n,m);
     cout<<"The Final Matrix is :"<<endl;
