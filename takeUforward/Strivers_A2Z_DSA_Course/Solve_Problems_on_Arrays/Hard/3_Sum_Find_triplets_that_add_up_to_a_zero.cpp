8c56988ea42540b5531798a6e9b564814eeb0e21
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-11-14 21:49:43 +0530

    arrray

---
+++ takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Hard/3_Sum_Find_triplets_that_add_up_to_a_zero.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Statement: Given an array of N integers, your task is to find unique triplets that add up to give a sum of zero. In short, you need to return an array of all the unique triplets [arr[a], arr[b], arr[c]] such that i!=j, j!=k, k!=i, and their sum is equal to zero.
+
+Pre-requisite: 2 Sum Problem
+
+Examples
+Example 1: 
+
+Input: nums = [-1,0,1,2,-1,-4]
+
+Output: [[-1,-1,2],[-1,0,1]]
+
+Explanation: Out of all possible unique triplets possible, [-1,-1,2] and [-1,0,1] satisfy the condition of summing up to zero with i!=j!=k
+
+Example 2:
+
+Input: nums=[-1,0,1,0]
+Output: Output: [[-1,0,1],[-1,1,0]]
+Explanation: Out of all possible unique triplets possible, [-1,0,1] and [-1,1,0] satisfy the condition of summing up to zero with i!=j!=k
+*/
