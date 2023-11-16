7ed3433512706df26932b304b29b6ee3f6b9ee04
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-11-16 20:00:43 +0530

    array

--- takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Easy/Longest_Subarray_with_sum_K_Postives_and_Negatives.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Easy/Longest_Subarray_with_sum_K_Postives_and_Negatives.cpp
@@ -1,20 +1,20 @@
 /*
 Problem Statement: Given an array and a sum k, we need to print the length of the longest subarray that sums to k.
 
-Examples
 Example 1:
 Input Format: N = 3, k = 5, array[] = {2,3,5}
 Result: 2
 Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.
 
 Example 2:
-Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
+Input Format: N = 3, k = 1, array[] = {-1, 1, 1}
 Result: 3
-Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+Explanation: The longest subarray with sum 1 is {-1, 1, 1}. And its length is 3.
 */
 
 
 #include <iostream>
+#include <map>
 using namespace std;
 
 //Brute Force Approach
@@ -41,6 +41,58 @@ int getLongestSubarray_1(vector<int> arr,int k)
     return len;
 }
 
+//Better Approach
+//Time Complexity: O(N2) approx., where N = size of the array.
+//Reason: We are using two nested loops, each running approximately N times.
+//Space Complexity: O(1) as we are not using any extra space.
+int getLongestSubarray_2(vector<int> arr,int k)
+{
+    int len = 0;
+    for(int i=0;i<arr.size();i++)
+    {
+        int sum = 0;
+        for(int j=i;j<arr.size();j++)
+        {
+            sum += arr[j];
+            if(sum == k)
+            {
+                len = max(len,j-i+1);
+            }
+        }
+    }
+    return len;
+}
+
+//Optimal Approach
+//Time Complexity: O(N) or O(N*logN) depending on which map data structure we are using, where N = size of the array.
+//Reason: For example, if we are using an unordered_map data structure in C++ the time complexity will be O(N)(though in the worst case, unordered_map takes O(N) to find an element and the time complexity becomes O(N2)) but if we are using a map data structure, the time complexity will be O(N*logN). The least complexity will be O(N) as we are using a loop to traverse the array.
+//Note: To know more about maps, please refer to this: Hashing | Maps | Time Complexity | Collisions | Division Rule of Hashing | Strivers A2Z DSA Course.
+//Space Complexity: O(N) as we are using a map data structure.
+int getLongestSubarray_3(vector<int> arr,int k)
+{
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+        if(sum == k)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+        int rem = sum - k;
+        if(preSumMap.find(rem) != preSumMap.end()) 
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        if(preSumMap.find(sum) == preSumMap.end()) 
+        {
+            preSumMap[sum] = i;
+        }
+    }
+    return maxLen;
+}
 
 int main()
 {
@@ -59,4 +111,6 @@ int main()
         arr.push_back(a);
     }
     cout<<"The length of the longest subarray is: "<<getLongestSubarray_1(arr,k)<<endl;
+    cout<<"The length of the longest subarray is: "<<getLongestSubarray_2(arr,k)<<endl;
+    cout<<"The length of the longest subarray is: "<<getLongestSubarray_3(arr,k)<<endl;
 }
