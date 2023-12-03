d520d329525a93a74568d75e4918690b2a87bac0
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-03 21:42:50 +0530

    array

--- takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Hard/Maximum_Product_Subarray_in_an_Array.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Hard/Maximum_Product_Subarray_in_an_Array.cpp
@@ -22,24 +22,72 @@ Explanation:
 #include <iostream>
 using namespace std;
 
+//Brute Force Approach
+//Time Complexity: O(N3)
+//Reason: We are using 3 nested loops for finding all possible subarrays and their product.
+//Space Complexity: O(1)
+//Reason: No extra data structure was used
 int max_product_subarray_1(vector<int> arr)
 {
-
+    int maxProd = INT_MIN;
+    for(int i=0;i<arr.size();i++)
+    {
+        for(int j=i;j<arr.size();j++)
+        {
+            int prod = 1;
+            for(int k=i;k<=j;k++)
+            {
+                prod *= arr[k]; 
+            }
+            maxProd = max(prod,maxProd);
+        }
+    }
+    return maxProd;
 }
 
+//Better Approach
+//Time Complexity: O(N2)
+//Reason: We are using two nested loops
+//Space Complexity: O(1)
+//Reason: No extra data structures are used for computation
 int max_product_subarray_2(vector<int> arr)
 {
-
+    int maxProd = INT_MIN;
+    for(int i=0;i<arr.size();i++)
+    {
+        int prod = 1;
+        for(int j=i;j<arr.size();j++)
+        {
+            prod *= arr[j];
+            maxProd = max(prod,maxProd);
+        }
+    }
+    return maxProd;
 }
 
+//optimal approach 1
+//Time Complexity: O(N), N = size of the given array.
+//Reason: We are using a single loop that runs for N times.
+//Space Complexity: O(1) as No extra data structures are used for computation.
 int max_product_subarray_3(vector<int> arr)
 {
-
+    int pre = 1, suff = 1;
+    int ans = INT_MIN;
+    for (int i = 0; i < arr.size(); i++) 
+    {
+        if (pre == 0) pre = 1;
+        if (suff == 0) suff = 1;
+        pre *= arr[i];
+        suff *= arr[arr.size() - i - 1];
+        ans = max(ans, max(pre, suff));
+    }
+    return ans;
 }
 
 int max_product_subarray_4(vector<int> arr)
 {
-
+    int prod = 1;
+    return prod;
 }
 
 int main()
@@ -56,9 +104,9 @@ int main()
         arr.push_back(a);
     }
 
-    cout<<"Toatl Inversion = "<<max_product_subarray_1(arr)<<endl;
-    cout<<"Toatl Inversion = "<<max_product_subarray_2(arr)<<endl;
-    cout<<"Toatl Inversion = "<<max_product_subarray_3(arr)<<endl;
-    cout<<"Toatl Inversion = "<<max_product_subarray_4(arr)<<endl;
+    cout<<"Maximum product subarray = "<<max_product_subarray_1(arr)<<endl;
+    cout<<"Maximum product subarray = "<<max_product_subarray_2(arr)<<endl;
+    cout<<"Maximum product subarray = "<<max_product_subarray_3(arr)<<endl;
+    cout<<"Maximum product subarray = "<<max_product_subarray_4(arr)<<endl;
     return 0;
 }
