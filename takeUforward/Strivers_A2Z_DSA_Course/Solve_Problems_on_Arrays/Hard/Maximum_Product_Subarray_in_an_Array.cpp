6e9f4602524ba9c79392b759fd0af17a14465a94
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-03 21:50:18 +0530

    array

--- takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Hard/Maximum_Product_Subarray_in_an_Array.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Solve_Problems_on_Arrays/Hard/Maximum_Product_Subarray_in_an_Array.cpp
@@ -94,8 +94,8 @@ int max_product_subarray_4(vector<int> arr)
     int prod1 = arr[0],prod2 = arr[0],result = arr[0];
     for(int i=1;i<arr.size();i++) 
     {
-        int temp = max({arr[i],prod1*arr[i],prod2*arr[i]});
-        prod2 = min({arr[i],prod1*arr[i],prod2*arr[i]});
+        int temp = max(arr[i],max(prod1*arr[i],prod2*arr[i]));
+        prod2 = min(arr[i],min(prod1*arr[i],prod2*arr[i]));
         prod1 = temp;
         result = max(result,prod1);
     }
