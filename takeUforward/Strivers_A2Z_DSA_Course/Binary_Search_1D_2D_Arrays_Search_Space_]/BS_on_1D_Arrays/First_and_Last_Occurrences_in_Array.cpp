c1da01edceeaed17bd10564dfe742fd54417dc9a
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-10 20:12:26 +0530

    binary

--- takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/First_and_Last_Occurrences_in_Array.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/First_and_Last_Occurrences_in_Array.cpp
@@ -20,6 +20,9 @@ Explanation: The target value is not present in the array. So, we have returned 
 #include <iostream>
 using namespace std;
 
+//Time Complexity: O(N), N = size of the given array
+//Reason: We are traversing the entire array.
+//Space Complexity: O(1) as we are not using any extra space.
 pair<int,int> first_and_last_occurrence_1(vector<int> arr, int x)
 {
     int first = -1;
@@ -41,6 +44,114 @@ pair<int,int> first_and_last_occurrence_1(vector<int> arr, int x)
     return make_pair(first,last);
 }
 
+//Time Complexity: O(2*logN), where N = size of the given array.
+//Reason: We are basically using a lower-bound and upper-bound algorithm.
+//Space Complexity: O(1) as we are using no extra space.
+int Lower_Bound(vector<int> arr,int target)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+    int ans = arr.size();
+    while (low<=high)
+    {
+        int mid = (low + high) / 2;
+        if(arr[mid]>=target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+int Upper_Bound(vector<int> arr,int target)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+    int ans = arr.size();
+    while (low<=high)
+    {
+        int mid = (low + high) / 2;
+        if(arr[mid]>target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+pair<int,int> first_and_last_occurrence_2(vector<int> arr, int x)
+{
+    int first = Lower_Bound(arr,x);
+    int last = Upper_Bound(arr,x);
+    return make_pair(first,last-1);
+}
+
+//Time Complexity: O(2*logN), where N = size of the given array.
+//Reason: We are basically using the binary search algorithm twice.
+//Space Complexity: O(1) as we are using no extra space.
+int first_occurence(vector<int> arr, int x)
+{
+    int first = -1;
+    int low = 0;
+    int high = arr.size() - 1;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        if(arr[mid] == x)
+        {
+            first = mid;
+            high = mid - 1;
+        }
+        else if(arr[mid]<x) 
+        {
+            low = mid + 1;
+        }
+        else 
+        {
+            high = mid - 1;
+        }
+    }
+    return first;
+}
+int last_occurence(vector<int> arr, int x)
+{
+    int last = -1;
+    int low = 0;
+    int high = arr.size() - 1;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        if(arr[mid] == x)
+        {
+            last = mid;
+            low = mid + 1;
+        }
+        else if(arr[mid]<x) 
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return last;
+}
+pair<int,int> first_and_last_occurrence_3(vector<int> arr, int x)
+{
+    int first = first_occurence(arr,x);
+    int last = last_occurence(arr,x);
+    return make_pair(first,last);
+}
+
 int main()
 {
     int n;
@@ -59,6 +170,8 @@ int main()
     cin>>search;
     pair<int,int> ans = first_and_last_occurrence_1(arr,search);
     cout <<"First Occurrence = "<<ans.first<<"\nLast Occurrence = "<<ans.second<<endl;
-    //cout <<"smallest index such that arr[i] >= x => "<<Lower_Bound_2(arr,search)<<endl;
-    //cout <<"smallest index such that arr[i] >= x => "<<Lower_Bound_3(arr,search)<<endl;
+    pair<int,int> ans1 = first_and_last_occurrence_2(arr,search);
+    cout <<"First Occurrence = "<<ans1.first<<"\nLast Occurrence = "<<ans1.second<<endl;
+    pair<int,int> ans2 = first_and_last_occurrence_3(arr,search);
+    cout <<"First Occurrence = "<<ans2.first<<"\nLast Occurrence = "<<ans2.second<<endl;
 }
