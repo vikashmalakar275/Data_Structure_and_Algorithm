7b10329dbbabba4a8fbd37a21470e841662bf7bf
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-10 21:37:33 +0530

    binary search

--- takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Search_Element_in_Rotated_Sorted_Array_II.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Search_Element_in_Rotated_Sorted_Array_II.cpp
@@ -1,16 +1,18 @@
 /*
-Problem Statement: Given an integer array arr of size N, sorted in ascending order (with distinct values) and a target value k. Now the array is rotated at some pivot point unknown to you. Find the index at which k is present and if k is not present return -1.
+Problem Statement: Given an integer array arr of size N, sorted in ascending order (may contain duplicate values) and a target value k. Now the array is rotated at some pivot point unknown to you. Return True if k is present and otherwise, return False. 
+
+Pre-requisite: Search Element in Rotated Sorted Array I & Binary Search algorithm
 
 Examples
 Example 1:
-Input Format: arr = [4,5,6,7,0,1,2,3], k = 0
-Result: 4
-Explanation: Here, the target is 0. We can see that 0 is present in the given rotated sorted array, nums. Thus, we get output as 4, which is the index at which 0 is present in the array.
+Input Format: arr = [7, 8, 1, 2, 3, 3, 3, 4, 5, 6], k = 3
+Result: True
+Explanation: The element 3 is present in the array. So, the answer is True.
 
 Example 2:
-Input Format: arr = [4,5,6,7,0,1,2], k = 3
-Result: -1
-Explanation: Here, the target is 3. Since 3 is not present in the given rotated sorted array. Thus, we get the output as -1.
+Input Format: arr = [7, 8, 1, 2, 3, 3, 3, 4, 5, 6], k = 10
+Result: False
+Explanation: The element 10 is not present in the array. So, the answer is False.
 */
 #include <iostream>
 using namespace std;
@@ -19,23 +21,19 @@ using namespace std;
 //Reason: We have to iterate through the entire array to check if the target is present in the array.
 //Space Complexity: O(1)
 //Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
-int rotated_sorted_1(vector<int> arr, int x)
+bool rotated_sorted_1(vector<int> arr, int x)
 {
     for(int i=0;i<arr.size();i++)
     {
         if(arr[i] == x)
         {
-            return i;
+            return true;
         }
     }
-    return -1;
+    return false;
 }
 
-//Time Complexity: O(logN), N = size of the given array.
-//Reason: We are using binary search to search the target.
-//Space Complexity: O(1)
-//Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
-int rotated_sorted_2(vector<int> arr, int x)
+bool rotated_sorted_2(vector<int> arr, int x)
 {
     int low = 0;
     int high = arr.size() - 1;
@@ -44,7 +42,13 @@ int rotated_sorted_2(vector<int> arr, int x)
         int mid = (low + high) / 2;
         if(arr[mid] == x)
         {
-            return mid;
+            return true;
+        }
+        if(arr[low]==arr[mid] && arr[mid]==arr[high])
+        {
+            low = low + 1;
+            high = high - 1;
+            continue;
         }
         if(arr[low] <= arr[mid])
         {
@@ -69,7 +73,7 @@ int rotated_sorted_2(vector<int> arr, int x)
             }
         }
     }
-    return -1;
+    return false;
 }
 
 int main()
@@ -88,6 +92,20 @@ int main()
     int search;
     cout<<"Enter the searched element :"<<endl;
     cin>>search;
-    cout <<"Element "<<search<<" found is at index = "<<rotated_sorted_1(arr,search)<<endl;
-    cout <<"Element "<<search<<" found is at index = "<<rotated_sorted_2(arr,search)<<endl;
+    if(rotated_sorted_1(arr,search))
+    {
+        cout<<"Element is present."<<endl;
+    }
+    else
+    {
+        cout<<"Element is not present."<<endl;
+    }
+    if(rotated_sorted_2(arr,search))
+    {
+        cout<<"Element is present."<<endl;
+    }
+    else
+    {
+        cout<<"Element is not present."<<endl;
+    }
 }
