da948b29f002ac43e8c499a793e2b6b2ce824def
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-14 22:18:37 +0530

    binary search

---
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Search_Single_Element_in_a_sorted_array.cpp
@@ -0,0 +1,17 @@
+/*
+Problem Statement: Given an array of N integers. Every number in the array except one appears twice. Find the single number in the array.
+
+Pre-requisite: Binary Search Algorithm
+
+Examples
+Example 1:
+Input Format: arr[] = {1,1,2,2,3,3,4,5,5,6,6}
+Result: 4
+Explanation: Only the number 4 appears once in the array.
+
+Example 2:
+
+Input Format: arr[] = {1,1,3,5,5}
+Result: 3
+Explanation: Only the number 3 appears once in the array.
+*/
