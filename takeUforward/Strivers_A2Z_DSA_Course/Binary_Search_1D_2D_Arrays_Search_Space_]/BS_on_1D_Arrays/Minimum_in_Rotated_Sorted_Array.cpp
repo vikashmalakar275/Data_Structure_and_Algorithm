eeea4f6fdaac72d006e4e7dfb361961a29cb9bd5
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-16 16:17:43 +0530

    binary search

--- takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Minimum_in_Rotated_Sorted_Array.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Minimum_in_Rotated_Sorted_Array.cpp
@@ -14,3 +14,95 @@ Input Format: arr = [3,4,5,1,2]
 Result: 1
 Explanation: Here, the element 1 is the minimum element in the array.
 */
+#include <iostream>
+using namespace std;
+
+//Time Complexity: O(N), N = size of the given array.
+//Reason: We have to iterate through the entire array to check if the target is present in the array.
+//Space Complexity: O(1)
+//Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+int minElement(vector<int> arr)
+{
+    int min = arr[0];
+    for(int i=1;i<arr.size();i++)
+    {
+        if(min>arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+//Time Complexity: O(logN), N = size of the given array.
+//Reason: We are basically using binary search to find the minimum.
+//Space Complexity: O(1)
+//Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+int minElement_1(vector<int> arr)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+    int min_no = arr[0];
+    while(low<=high)
+    {
+        int mid = (low + high) / 2;
+        if(arr[low]<=arr[mid])
+        {
+            min_no = min(min_no,arr[low]);
+            low = mid + 1;
+        }
+        else
+        {
+            min_no = min(min_no,arr[mid]);
+            high = mid - 1;
+        }
+    }
+    return min_no;
+}
+
+//Time Complexity: O(logN), N = size of the given array.
+//Reason: We are basically using binary search to find the minimum. 
+//Space Complexity: O(1)
+//Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+int minElement_2(vector<int> arr)
+{
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[low] <= arr[high]) 
+        {
+            ans = min(ans, arr[low]);
+            break;
+        }
+        if (arr[low] <= arr[mid]) 
+        {
+            ans = min(ans, arr[low]);
+            low = mid + 1;
+        }
+        else 
+        {
+            ans = min(ans, arr[mid]);
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of array :"<<endl;
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter the element of array ;"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        arr.push_back(a);
+    }
+    cout <<"Minimum Element = "<<minElement(arr)<<endl;
+    cout <<"Minimum Element = "<<minElement_1(arr)<<endl;
+    cout <<"Minimum Element = "<<minElement_2(arr)<<endl;
+}
