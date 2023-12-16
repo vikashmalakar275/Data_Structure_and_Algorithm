8c1a27a955cf15384604ac103635c311a98097a9
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-16 17:10:08 +0530

    binary search

--- takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Search_Single_Element_in_a_sorted_array.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Search_Single_Element_in_a_sorted_array.cpp
@@ -15,3 +15,90 @@ Input Format: arr[] = {1,1,3,5,5}
 Result: 3
 Explanation: Only the number 3 appears once in the array.
 */
+
+#include <iostream>
+using namespace std;
+
+//Time Complexity: O(N), N = size of the given array.
+//Reason: We are traversing the entire array.
+//Space Complexity: O(1) as we are not using any extra space.
+int searchSingleElement_1(vector<int> arr)
+{
+    for(int i=0;i<arr.size();i++)
+    {
+        if(i==0 && arr[i] != arr[i+1])
+        {
+            return arr[i];
+        }
+        else if(i==arr.size()-1 && arr[i] != arr[i-1])
+        {
+            return arr[i];
+        }
+        else if(arr[i] != arr[i+1] && arr[i] != arr[i-1])
+        {
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+//Time Complexity: O(N), N = size of the given array.
+//Reason: We are traversing the entire array.
+//Space Complexity: O(1) as we are not using any extra space.
+int searchSingleElement_2(vector<int> arr)
+{
+    int ans = 0;
+    for(int i=0;i<arr.size();i++)
+    {
+        ans = ans^arr[i];
+    }
+    return ans;
+}
+
+//Time Complexity: O(logN), N = size of the given array.
+//Reason: We are basically using the Binary Search algorithm.
+//Space Complexity: O(1) as we are not using any extra space.
+int searchSingleElement_3(vector<int> arr)
+{
+    int n = arr.size();
+    if (n == 1) return arr[0];
+    if (arr[0] != arr[1]) return arr[0];
+    if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
+
+    int low = 1, high = n - 2;
+    while (low <= high) 
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1])
+        {
+            return arr[mid];
+        }
+        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1]) || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) 
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of array :"<<endl;
+    cin>>n;
+    vector<int> arr;
+    cout<<"Enter the element of array ;"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        arr.push_back(a);
+    }
+    cout <<"Single Element = "<<searchSingleElement_1(arr)<<endl;
+    cout <<"Single Element = "<<searchSingleElement_2(arr)<<endl;
+    cout <<"Single Element = "<<searchSingleElement_3(arr)<<endl;
+}
