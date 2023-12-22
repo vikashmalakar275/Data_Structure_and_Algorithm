18d53d81e65939e25910db06cc9e6c7fdf2ed5e2
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-22 14:03:22 +0530

    binary search

--- takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Peak_element_in_Array.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_1D_Arrays/Peak_element_in_Array.cpp
@@ -45,6 +45,46 @@ int peakElement_1(vector<int> arr)
     return -1;
 }
 
+//Time Complexity: O(logN), N = size of the given array.
+//Reason: We are basically using binary search to find the minimum.
+//Space Complexity: O(1)
+//Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+int peakElement_2(vector<int> arr)
+{
+    int n = arr.size();
+    if(n==1)
+    {
+        return 0;
+    }
+    if(arr[0]>arr[1])
+    {
+        return 0;
+    }
+    if(arr[n-1] > arr[n-2])
+    {
+        return n-1;
+    }
+    int low = 1;
+    int high = n - 2;
+    while(low<=high)
+    {
+        int mid = (low + high) / 2;
+        if(arr[mid - 1]< arr[mid] && arr[mid] > arr[mid+1])
+        {
+            return mid;
+        }
+        if(arr[mid] > arr[mid-1])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     int n;
