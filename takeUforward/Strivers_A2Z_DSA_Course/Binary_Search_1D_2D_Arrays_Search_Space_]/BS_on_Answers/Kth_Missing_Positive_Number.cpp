63dbf04df8fa6ea453f99ae6e3adb5cf4306a64c
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2023-12-28 18:14:17 +0530

    binary search

--- takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_Answers/Kth_Missing_Positive_Number.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_Answers/Kth_Missing_Positive_Number.cpp
@@ -14,6 +14,9 @@ Explanation: The missing numbers are 1, 2, 3, 5, 6, 8, 11, 12, ……, and so on
 #include <iostream>
 using namespace std;
 
+//Time Complexity: O(N), N = size of the given array.
+//Reason: We are using a loop that traverses the entire given array in the worst case.
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
 int kth_missing_number_1(vector<int> arr,int k)
 {
     for(int i=0;i<arr.size();i++)
@@ -28,6 +31,29 @@ int kth_missing_number_1(vector<int> arr,int k)
         }
     }
     return k;
+}
+
+//Time Complexity: O(logN), N = size of the given array.
+//Reason: We are using the simple binary search algorithm.
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
+int kth_missing_number_2(vector<int> arr,int k)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        int missing = arr[mid] - (mid+1);
+        if(missing<k)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return low+k;
 }
 
 int main()
@@ -47,5 +73,5 @@ int main()
     cout<<"Enther the nth missing number :"<<endl;
     cin>>k;
     cout<<k<<"th Missing Number : "<<kth_missing_number_1(arr,k)<<endl;
-    //cout<<"Least Capacity = "<<leastCapacity_1(arr,k)<<endl;
+    cout<<k<<"th Missing Number : "<<kth_missing_number_2(arr,k)<<endl;
 }
