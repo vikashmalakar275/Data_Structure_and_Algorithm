e30d0ad74984be95f92a527efe532906dc3764a6
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2024-01-09 16:19:23 +0530

    binary search

--- takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_2D_Arrays/Find_A_Peak_Element.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_2D_Arrays/Find_A_Peak_Element.cpp
@@ -33,4 +33,134 @@ Input: 'n' = 2, 'm' = 2
 Output: 1
 
 Sample Explanation: Only one peak element is present at [1, 0].
+
+Sample Input 1:
+2 2
+8 6
+10 1
+Sample Output 1:
+1       
+Explanation of sample output 1:
+For g = [[8,6],[10,1]],
+Answer = [1,0].
+There is only one peak element that is present at [1,0].
+Sample Input 2:
+3 3
+1 2 3
+4 5 6
+7 8 9   
+Sample Output 2:
+1
+Explanation of sample output 2:
+For g = [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+Answer = [2,2].
+There is only one peak element that is present at [2,2].
+Constraints:
+1 <= n, m <= 500    
+1 <= g[i][j] <= 10^9    
+Adjacent cells are distinct.
+Time Limit: 1 sec
 */
+#include <iostream>
+using namespace std;
+
+//Time Complexity: O(rows * columns) 
+//Auxiliary Space: O(1)
+pair<int,int> search_peak_1(vector<vector<int> > &arr)
+{
+    int row = arr.size();
+    int column = arr[0].size();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++) 
+        {
+            if (i > 0)
+            {
+                if (arr[i][j] < arr[i - 1][j])
+                {
+                    continue;
+                }
+            }
+            if (j < column - 1)
+            {
+                if (arr[i][j] < arr[i][j + 1])
+                {
+                    continue;
+                }
+            }
+            if (i < row - 1)
+            {
+                if (arr[i][j] < arr[i + 1][j])
+                {
+                    continue;
+                }
+            }
+            if (j > 0)
+            {
+                if (arr[i][j] < arr[i][j - 1])
+                {
+                    continue;
+                }
+            }
+            return make_pair(i,j);
+        }
+    }
+    return make_pair(-1,-1);
+}
+
+//Time Complexity: O(rows * log(columns)). We recur for half the number of columns. In every recursive call, we linearly search for the maximum in the current mid column.
+//Auxiliary Space: O(columns/2) for Recursion Call Stack 
+pair<int,int> search_peak_2(vector<vector<int> > &arr)
+{
+    int low = 0;
+    int high = arr[0].size() - 1;
+ 
+    while (low <= high)
+    {
+        int mid = (low + high)/2;
+        int ansrow = 0;
+        for (int r = 0; r < arr.size(); r++)
+        {
+            ansrow = arr[r][mid] >= arr[ansrow][mid] ? r : ansrow;
+        }
+        bool valid_left = mid - 1 >= low && arr[ansrow][mid - 1] > arr[ansrow][mid];
+        bool valid_right = mid + 1 <= high && arr[ansrow][mid + 1] > arr[ansrow][mid];
+        if (!valid_left && !valid_right)
+        {
+            return make_pair(ansrow, mid);
+        }
+        else if(valid_right)
+        {
+            low = mid+ 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return make_pair(-1,-1);
+}
+
+int main()
+{
+    int n,m;
+    cout<<"Enter the number of Row :"<<endl;
+    cin>>n;
+    cout<<"Enter the number of Column :"<<endl;
+    cin>>m;
+    vector<vector<int> > arr(n);
+    cout<<"Enter the Value in Array :"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        arr[i].resize(m);
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    pair<int,int> ans = search_peak_1(arr);
+    cout<<"Peak Element found at index = ("<<ans.first<<" , "<<ans.second<<")"<<endl;
+    pair<int,int> ans1 = search_peak_2(arr);
+    cout<<"Peak Element found at index = ("<<ans1.first<<" , "<<ans1.second<<")"<<endl;
+    return 0;
+}
