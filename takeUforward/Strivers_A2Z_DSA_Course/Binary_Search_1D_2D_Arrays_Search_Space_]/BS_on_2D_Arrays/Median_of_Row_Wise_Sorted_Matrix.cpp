f42410d38d29221b6aedae9b978e7b5e2ac53d39
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2024-01-09 18:51:33 +0530

    binary search

--- takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_2D_Arrays/Median_of_Row_Wise_Sorted_Matrix.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Binary_Search_1D_2D_Arrays_Search_Space_]/BS_on_2D_Arrays/Median_of_Row_Wise_Sorted_Matrix.cpp
@@ -25,3 +25,46 @@ Output: Median = 3
 Explanation: If we find the linear sorted array, the array becomes 1 1 2 2 3 3 4 5 7 8
 So, median = 3
 */
+#include <iostream>
+using namespace std;
+
+int median_1(vector<vector<int> > &arr)
+{
+    vector<int> arr1;
+    for(int i=0;i<arr.size();i++)
+    {
+        for(int j=0;j<arr[0].size();j++)
+        {
+            arr1.push_back(arr[i][j]);
+        }
+    }
+    sort(arr1.begin(),arr1.end());
+    return arr1[arr1.size()/2];
+}
+
+// int median_2(vector<vector<int> > &arr)
+// {
+    
+// }
+
+int main()
+{
+    int n,m;
+    cout<<"Enter the number of Row :"<<endl;
+    cin>>n;
+    cout<<"Enter the number of Column :"<<endl;
+    cin>>m;
+    vector<vector<int> > arr(n);
+    cout<<"Enter the Value in Array :"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        arr[i].resize(m);
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"Median = "<<median_1(arr)<<endl;
+    //cout<<"Median = "<<median_2(arr)<<endl;
+    return 0;
+}
