c2eb71ceb698044ff03d75f0ce41fd1d8ac4a143
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2024-01-16 21:04:43 +0530

    string

---
+++ takeUforward/Strivers_A2Z_DSA_Course/Strings_Basic_and_Medium/check_whether_one_string_is_a_rotation_of_other.cpp
@@ -0,0 +1,11 @@
+/*
+Given a string s1 and a string s2, write a function to check whether s2 is a rotation of s1. 
+
+Examples: 
+
+Input: S1 = ABCD, S2 = CDAB
+Output: Strings are rotations of each other
+
+Input: S1 = ABCD, S2 = ACBD
+Output: Strings are not rotations of each other
+*/
