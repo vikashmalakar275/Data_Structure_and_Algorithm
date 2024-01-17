e37d445d7562187ee854c749c55df5f7e73bd340
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2024-01-17 19:49:48 +0530

    string

--- takeUforward/Strivers_A2Z_DSA_Course/Strings_Basic_and_Medium/check_whether_one_string_is_a_rotation_of_other.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Strings_Basic_and_Medium/check_whether_one_string_is_a_rotation_of_other.cpp
@@ -14,6 +14,57 @@ Output: Strings are not rotations of each other
 #include <algorithm>
 using namespace std;
 
+//Approach-1
+//Time Complexity: O(n*n) in the worst case, where n is the length of the string.
+//Auxiliary Space: O(n)
+bool checkString(string& s1,string& s2,int indexFound,int Size)
+{
+  for(int i = 0;i<Size;i++)
+  {
+		if (s1[i] != s2[(indexFound + i) % Size])
+    {
+      return false;
+    }
+	}
+	return true;
+}
+
+string check_whether_string_rotation_of_other(string s1,string s2)
+{
+  if(s1.length() != s2.length())
+  {
+		return "s2 is not a rotation on s1";
+	}
+	else
+  {
+    vector<int> indexes;
+		int Size = s1.length();
+		char firstChar = s1[0];
+		for(int i=0; i<Size; i++)
+    {
+      if(s2[i] == firstChar)
+      {
+        indexes.push_back(i);
+			}
+		}
+		bool isRotation = false;
+		for (int idx : indexes)
+    {
+      isRotation = checkString(s1, s2, idx, Size);
+			if (isRotation)
+      {
+				break;
+      }
+		}
+		if(isRotation)
+    {
+      return "Strings are rotations of each other";
+    }
+	}
+  return "Strings are not rotations of each other";
+}
+
+
 int main()
 {
   string str1;
@@ -22,4 +73,5 @@ int main()
   string str2;
   cout<<"Enter the second string :"<<endl;
   getline(cin,str2);
+  cout<<check_whether_string_rotation_of_other(str1,str2)<<endl;
 }
