8de8b5180723326aa7df8afb4ab880f349b338cb
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2024-01-17 19:25:04 +0530

    string

--- takeUforward/Strivers_A2Z_DSA_Course/Strings_Basic_and_Medium/check_whether_one_string_is_a_rotation_of_other.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Strings_Basic_and_Medium/check_whether_one_string_is_a_rotation_of_other.cpp
@@ -13,35 +13,13 @@ Output: Strings are not rotations of each other
 #include <iostream>
 #include <algorithm>
 using namespace std;
-bool CheckAnagrams(string str1, string str2)
-{
-  // when both of the strings have different lengths
-  if (str1.length() != str2.length())
-    return false;
- 
-  int freq[26] = {0};
-  for (int i = 0; i < str1.length(); i++)
-  {
-    freq[str1[i] - 'A']++;
-  }
-  for (int i = 0; i < str2.length(); i++)
-  {
-    freq[str2[i] - 'A']--;
-  }
-  for (int i = 0; i < 26; i++)
-  {
-    if (freq[i] != 0)
-      return false;
-  }
-  return true;
-}
+
 int main()
 {
-  string Str1 = "INTEGER";
-  string Str2 = "TEGERNI";
-  if(CheckAnagrams(Str1, Str2))
-  cout << "True" << endl;
-  else
-  cout<<"False"<<endl;
-  return 0;
+  string str1;
+  cout<<"Enter the first string :"<<endl;
+  getline(cin,str1);
+  string str2;
+  cout<<"Enter the second string :"<<endl;
+  getline(cin,str2);
 }
