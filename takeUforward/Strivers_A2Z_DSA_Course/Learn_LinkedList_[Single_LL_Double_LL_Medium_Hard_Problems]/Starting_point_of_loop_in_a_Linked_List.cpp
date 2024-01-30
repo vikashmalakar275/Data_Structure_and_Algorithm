b6af0ed2cff6070a15deeafe006f46648a83929b
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2024-01-30 13:20:52 +0530

    Linked List

--- takeUforward/Strivers_A2Z_DSA_Course/Learn_LinkedList_[Single_LL_Double_LL_Medium_Hard_Problems]/Starting_point_of_loop_in_a_Linked_List.cpp
+++ takeUforward/Strivers_A2Z_DSA_Course/Learn_LinkedList_[Single_LL_Double_LL_Medium_Hard_Problems]/Starting_point_of_loop_in_a_Linked_List.cpp
@@ -24,7 +24,7 @@ Explanation:
 */
 
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 //Linked List Creation Logic Start
@@ -107,42 +107,31 @@ void displayLinkedList(Node* head,int size)
     cout<<endl;
 }
 
-//Time Complexity: O(N * 2 * log(N) )The algorithm traverses the linked list once, performing hashmap insertions and searches in the while loop for each node. The insertion and search operations in the unordered_map have a worst-case time complexity of O(log(N)). As the loop iterates through N nodes, the total time complexity is determined by the product of the traversal (O(N)) and the average-case complexity of the hashmap operations (insert and search), resulting in O(N * 2 * log(N)). 
-//Hashmaps and their time complexities are discussed in more detail here. 
-//Space Complexity: O(N) The code uses a hashmap/dictionary to store encountered nodes, which can take up to O(N) additional space, where ‘n’ is the number of nodes in the list. Hence, the spacecomplexity is O(N) due to the use of the map to track nodes.
-bool detectLoop(Node* head) 
+Node *starting_point_of_loop_1(Node* head)
 {
     Node* temp = head;
-    unordered_map<Node*, int> nodeMap;
-    while (temp != nullptr)
+    unordered_set<Node*> st;
+    while(temp != NULL) 
     {
-        if(nodeMap.find(temp) != nodeMap.end())
-        {
-            return true;
+        if(st.find(temp) != st.end())
+        { 
+            return temp;
         }
-        nodeMap[temp] = 1;
-        temp = temp->next;  
+        st.insert(temp);
+        temp = temp->next;
     }
-    return false;
+    return NULL;
 }
 
-//Time Complexity: O(N), where N is the number of nodes in the linked list. This is because in the worst-case scenario, the fast pointer, which moves quicker, will either reach the end of the list (in case of no loop) or meet the slow pointer (in case of a loop) in a linear time relative to the length of the list.
-//The key insight into why this is O(N) and not something slower is that each step of the algorithm reduces the distance between the fast and slow pointers (when they are in the loop) by one. Therefore, the maximum number of steps needed for them to meet is proportional to the number of nodes in the list.
-//Space Complexity : O(1) The code uses only a constantamount of additionalspace, regardless of the linked list’s length. This is achieved by using two pointers (slow and fast) to detect the loop without any significant extra memory usage, resulting in constantspace complexity, O(1).
-bool detectCycle(Node* head)
+int getLoopStartingPostion(Node* head,Node* res)
 {
-    Node* slow = head;
-    Node* fast = head;
-    while (fast != nullptr && fast->next != nullptr) 
+    int pos = 1;
+    while(head != res)
     {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast) 
-        {
-            return true;
-        }
+        pos++;
+        head = head->next;
     }
-    return false;
+    return pos;
 }
 
 int main() 
@@ -160,13 +149,7 @@ int main()
     cout<<"Linked List After Cycle creation :"<<endl;
     displayLinkedList(head,size);
 
-    if(detectLoop(head) && detectCycle(head))
-    {
-        cout<<"Loop detected in the linked list."<<endl;
-    } 
-    else 
-    {
-        cout<<"No loop detected in the linked list."<<endl;
-    }
+    Node* res = starting_point_of_loop_1(head);
+    cout<<"Loop started from position : "<<getLoopStartingPostion(head,res)<<endl;
     return 0;
 }
