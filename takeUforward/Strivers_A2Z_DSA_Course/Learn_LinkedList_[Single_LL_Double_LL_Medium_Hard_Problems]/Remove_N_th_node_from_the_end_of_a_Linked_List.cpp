43c3387bb891c261ef725c53aaae1a2551cfdfc4
vikash malakar <42803815+vikashmalakar275@users.noreply.github.com>
2024-02-02 14:15:03 +0530

    Linked List

---
+++ takeUforward/Strivers_A2Z_DSA_Course/Learn_LinkedList_[Single_LL_Double_LL_Medium_Hard_Problems]/Remove_N_th_node_from_the_end_of_a_Linked_List.cpp
@@ -0,0 +1,132 @@
+/*
+Problem Statement: Given a linked list and an integer N, the task is to delete the Nth node from the end of the linked list and print the updated linked list.
+
+Examples
+Example 1:
+Input Format: 5->1->2, N=2
+Result: 5->2
+Explanation: The 2nd node from the end of the linked list is 1. Therefore, we get this result after removing 1 from the linked list.
+
+Example 2:
+Input Format: 1->2->3->4->5, N=3
+Result: 1->2->4->5
+Explanation: The 3rd node from the end is 3, therefore, we remove 3 from the linked list.
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+//Linked List Creation Logic Start
+struct Node {
+    int data;
+    Node* next;
+};
+
+// Function to create a new node
+Node* createNode(int data)
+{
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+Node *createLinkedList(Node* head)
+{
+    cout<<"Enter the size of the newly created linked list."<<endl;
+    int size;
+    cin>>size;
+    while(size-->0)
+    {
+        Node* new_node = new Node;
+        cout << "Enter data: ";
+        cin >> new_node->data;
+        new_node->next = NULL;
+        if(head == NULL)
+        {
+            head = new_node;
+        }
+        else
+        {
+            Node* temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = new_node;
+        }
+    }
+    return head;
+}
+
+// Function to display the linked list
+void displayLinkedList(Node* head) 
+{
+    Node* temp = head;
+    while (temp != nullptr) 
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "nullptr" << endl;
+}
+
+//Linked List Creation Logic End
+
+Node* remove_Nth_node_from_end_1(Node* head,int n)
+{
+    if(head == NULL)
+    {
+        return NULL;
+    }
+    int len = 0;
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+    cout<<"Length of LinkedList = "<<len<<endl;
+    
+    if(len == n)
+    {
+        Node* newHead = head->next;
+        delete (head);
+        return newHead;
+    }
+
+    int Nth_node = len-n;
+    temp = head;
+    cout<<"Position from front = "<<Nth_node+1<<endl;
+    while(temp != NULL)
+    {
+        Nth_node--;
+        if(Nth_node == 0)
+        {
+            break;
+        }
+        temp = temp->next;
+    }
+    Node* del = temp->next;
+    temp->next = temp->next->next;
+    delete(del);
+    return head;
+}
+
+int main() 
+{
+    Node* head = nullptr; // Initialize the head of the linked list
+    cout<<"Create a Linked List:"<<endl;
+    head = createLinkedList(head);
+    cout<<"Linked List After creation :"<<endl;
+    displayLinkedList(head);
+
+    int n;
+    cout<<"Enter the position you want to delete from END :"<<endl;
+    cin>>n;
+    head = remove_Nth_node_from_end_1(head,n);
+    cout<<"Linked List after delete Nth Node from the end"<<endl;
+    displayLinkedList(head);
+    return 0;
+}
